38643734a3e1b63bc1d229919fe12a2a2f3370ff
Jairinho061 <jairb3436@gmail.com>
2025-04-11 16:50:08 -0300

    Update NucleoCalculosTrigonometricos.c

--- NucleoCalculosTrigonometricos.c
+++ NucleoCalculosTrigonometricos.c
@@ -10,33 +10,40 @@ int main() {
     double angulo_graus, angulo_rad;
     double seno, cosseno, tangente;
 
-    printf("Escolha qual razão trigonométrica você quer:\n");
+    printf("Escolha qual razÃ£o trigonomÃ©trica vocÃª quer:\n");
     printf("1 - SENO\n2 - COSSENO\n3 - TANGENTE\n");
     scanf("%d", &escolha);
  
-    printf("Digite o ângulo em graus: ");
+    printf("Digite o Ã¢ngulo em graus: ");
     scanf("%lf", &angulo_graus);
 
-    angulo_rad = angulo_graus * (PI / 180.0);  // converte pra radiano
-   // escolhe  o tipo de resolução
+   // escolhe  o tipo de resoluÃ§Ã£o
     switch (escolha) {
         case 1:
+            angulo_rad = angulo_graus * (PI / 180.0);  // converte pra radiano
             seno = sin(angulo_rad);
             printf("Seno: %.4f\n", seno);
             break;
 
         case 2:
+            angulo_rad = angulo_graus * (PI / 180.0);
             cosseno = cos(angulo_rad);
             printf("Cosseno: %.4f\n", cosseno);
             break;
 
         case 3:
-            tangente = tan(angulo_rad);
-            printf("Tangente: %.4f\n", tangente);
+            if(angulo_graus == 90){
+                printf("Tangente indefinida.");
+            }
+            else{
+                angulo_rad = angulo_graus * (PI / 180.0); 
+                tangente = tan(angulo_rad);
+                printf("Tangente: %.4f\n", tangente);
+            }
             break;
 
         default:
-            printf("Opção inválida.\n");
+            printf("OpÃ§Ã£o invÃ¡lida.\n");
     }
 
     return 0;
